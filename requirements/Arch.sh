8bb99ec24a234e9f9a5364f846de87f6237b7360
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-07-03 22:30:24 +0200

    Create Arch.sh

---
+++ requirements/Arch.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "[+] Install dependencies packages"
+sudo pacman -S virtualbox ansible vagrant python-pywinrm
+
+echo "[+] Install Vagrant plugins"
+VAGRANT_DISABLE_STRICT_DEPENDENCY_ENFORCEMENT=1 vagrant plugin install winrm
+VAGRANT_DISABLE_STRICT_DEPENDENCY_ENFORCEMENT=1 vagrant plugin install winrm-elevated
+VAGRANT_DISABLE_STRICT_DEPENDENCY_ENFORCEMENT=1 vagrant plugin install winrm-fs
+
+echo "[+] Install Ansible plugins"
+ansible-galaxy collection install community.windows
+ansible-galaxy collection install ansible.windows
