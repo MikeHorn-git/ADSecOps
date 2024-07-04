37520fcbd6f6311d215007935de64a2989b5ff7c
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-07-04 09:58:37 +0000

    Add files via upload

---
+++ requirements/Debian.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "[+] Install dependencies packages"
+sudo apt update; sudo apt install virtualbox ansible vagrant python3-winrm -y
+
+echo "[+] Install Vagrant plugins"
+vagrant plugin install winrm
+vagrant plugin install winrm-elevated
+vagrant plugin install winrm-fs
+
+echo "[+] Install Ansible plugins"
+ansible-galaxy collection install community.windows
+ansible-galaxy collection install ansible.windows
